60db6fc9a0e87962b2ac87f7f2756399ab3d862e
Daniel Christiansen <dmaxchristiansen@gmail.com>
2022-12-19 16:31:25 -0700

    Run formatter

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,7 +2,7 @@ const Results = ({ userScore }) => (
   <div className="container text-center">
     <div className="fs-1">Time's Up!</div>
     <div className="fs-3 fw-bold mb-2">You Got</div>
-    <div style={{fontSize: 80}}>{userScore}</div>
+    <div style={{ fontSize: 80 }}>{userScore}</div>
     <div className="fs-3 fw-bold mb-3">Anagrams</div>
   </div>
 );
